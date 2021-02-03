96bde3bafaa1c74eb8c34847acc05a236593bcb4
Kelvin Yap <kelvin.yap@9spokes.com>
2021-02-03 16:56:03 +1300

    Add a ECS cluster in a VPC. Add a Python and a Node Fargate services.

--- lib/simple-cdk-stack.ts
+++ lib/simple-cdk-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 
 export class SimpleCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,5 +13,36 @@ export class SimpleCdkStack extends cdk.Stack {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
+
+    const vpc = new ec2.Vpc(this, "SimpleVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "SimpleCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Python Fargate service and make it private
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "SimplePythonFargateService", {
+      cluster: cluster, // Required
+      cpu: 256, // Default is 256
+      desiredCount: 1, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 512, // Default is 512
+      publicLoadBalancer: false // Default is false
+    });
+
+    // Create a load-balanced Node Fargate service and make it private
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "SimpleNodeFargateService", {
+      cluster: cluster, // Required
+      cpu: 256, // Default is 256
+      desiredCount: 1, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 512, // Default is 512
+      publicLoadBalancer: true // Default is false
+    });
+
+
+
   }
 }
