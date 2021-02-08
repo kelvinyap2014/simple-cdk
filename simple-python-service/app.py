a96d33860af6921f728549bad562bb6619056e22
Kelvin Yap <kelvin.yap@9spokes.com>
2021-02-08 22:48:14 +1300

    Python service tested on localstack SQS & S3

--- simple-python-service/app.py
+++ simple-python-service/app.py
@@ -1,36 +1,89 @@
-from sqs_listener import SqsListener
+import localstack_client.session
+import botocore
 import boto3
+import time
+import json
+from decouple import config
+import logging
+import sys
 
-class MyListener(SqsListener):
-    def handle_message(self, body, attributes, messages_attributes):
-        print(body)
-        put_to_s3(body["Message"], body["File"])
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        # logging.FileHandler("debug.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
-def put_to_s3(message, file_name):
-    print("Message is: " + message)
+def ensure_aws(service):
+    if 'true' == config('PY_DEV'):
+        session = localstack_client.session.Session(localstack_host=config('PY_HOST'))
+        return session.client(service)
+    else:
+        return boto3.resource(service)
 
+def ensure_queue(sqs, queueName):
+    queue = None
+    try:
+        queue = sqs.get_queue_url(QueueName=queueName)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'AWS.SimpleQueueService.NonExistentQueue':
+            logging.info('Queue [' + queueName + '] not exists. Create one.')
+            sqs.create_queue(QueueName=queueName)
+            queue = sqs.get_queue_url(QueueName=queueName)
+    return queue
+
+def process_queue(sqs, queueUrl):
+    message = sqs.receive_message(QueueUrl=queueUrl)
+    if 'Messages' in message.keys():
+        for m in message['Messages']:
+            logging.info('Found message --> ' + m['Body'])
+            body = json.loads(m['Body'])
+            put_to_s3(body['Message'], body['File'])
+            sqs.delete_message(QueueUrl=queueUrl, ReceiptHandle=m['ReceiptHandle'])
+        process_queue(sqs, queueUrl)
+    else:
+        logging.info(queueUrl + ' has no more message')
+
+def ensure_bucket(s3):
     # Retrieve the list of existing buckets
-    s3_client = boto3.client("s3")
-    response = s3_client.list_buckets()
+    response = s3.list_buckets()
 
     # Output the bucket names
-    bucket_name = ""
-    print("Existing buckets:")
-    for bucket in response["Buckets"]:
-        print(f'  {bucket["Name"]}')
-        if(bucket["Name"].startswith("simplecdkstack-simplebucket")):
-            bucket_name = bucket["Name"]
-    print(f"found - {bucket_name}")
+    bucket_name = ''
+    logging.info('Checking existing buckets:')
+    for bucket in response['Buckets']:
+        logging.info(f'  {bucket["Name"]}')
+        if(bucket['Name'].startswith(config('PY_BUCKET_NAME'))):
+            bucket_name = bucket['Name']
+    if bucket_name != '':
+        logging.info(f'Found bucket - {bucket_name}')
+    else:
+        logging.info('Bucket [' + config('PY_BUCKET_NAME') + '] not exists. Create one.')
+        bucket_name = config('PY_BUCKET_NAME')
+        s3.create_bucket(Bucket=bucket_name)
+
+    return bucket_name
+
+def put_to_s3(message, file_name):
+    logging.info('Message is: ' + message)
+    
+    s3 = ensure_aws('s3')
+    bucket_name = ensure_bucket(s3)
 
     # Create file
     with open(file_name, 'w') as f:
         f.write(str(message))
 
     # Talk to S3
-    s3_resource = boto3.resource('s3')
-    s3_resource.Bucket(bucket_name).upload_file(Filename=file_name, Key=file_name)
-    print(f"Uploaded file to S3 - {file_name}")
+    s3.upload_file(file_name, bucket_name, file_name)
+    logging.info(f'Uploaded file to S3 - {file_name}')
 
-listener = MyListener('SimpleQueue', error_queue='ErrorQueue', region_name='us-east-2')
-print("Simple Python Service listening on AWS SQS")
-listener.listen()
+
+sqs = ensure_aws('sqs')
+queue = ensure_queue(sqs, config('PY_QUEUE_NAME'))
+logging.info('Simple Python Service listening on AWS SQS')
+while(True):
+	process_queue(sqs, queue['QueueUrl'])
+	time.sleep(int(config('PY_INTERVAL')))
